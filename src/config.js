53c7e88726767c5328d6d212bfeab4325d0b4aea
fschweikhardt <schweikhardt.web@gmail.com>
2020-12-29 15:35:53 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default { 
     API_ENDPOINT_FOLDERS: 'http://localhost:9000/api/folders',
-    API_ENDPOINT_NOTES: 'http://localhost:9000/',
+    API_ENDPOINT_NOTES: 'http://localhost:9000/api/notes',
     API_TOKEN: '123456789'
 }
