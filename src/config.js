2b79c2bc49b73f344dcfcdc30c76dac19e6d5271
fschweikhardt <schweikhardt.web@gmail.com>
2021-01-02 20:40:17 -0500

    prep for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
+//eslint-disable-next-line
 export default { 
+    //eslint-disable-next-line
     API_ENDPOINT: 'https://fathomless-basin-45210.herokuapp.com/api',
+    //eslint-disable-next-line
     API_TOKEN: process.env.API_KEY || '123456789'
+    //eslint-disable-next-line
 }
+//eslint-disable-next-line
