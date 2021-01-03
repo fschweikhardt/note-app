e7dad1fa47fe8f91d92872a214be5919b7e9413a
fschweikhardt <schweikhardt.web@gmail.com>
2021-01-02 22:22:41 -0500

    Update Note.js

--- src/Note.js
+++ src/Note.js
@@ -47,12 +47,6 @@ class Note extends React.Component {
                 'Authorization': `Bearer ${config.API_TOKEN}`
                 },
             })
-            .then(res => {
-                if (!res.ok) {
-                    return res.json().then(e => Promise.reject(e))
-                }
-                return res.json()  
-            })
             .then(this.context.deleteNote(noteId))
             .then(this.props.onDeleteNote())
             .catch(error => {
